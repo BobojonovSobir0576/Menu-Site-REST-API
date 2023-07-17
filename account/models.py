410b62a5ec294719ded221076de3e0a51d31453c
BobojonovSobir0576 <dweb0987654321@gmail.com>
2023-07-17 13:18:08 +0500

    Finished

--- account/models.py
+++ account/models.py
@@ -42,7 +42,7 @@ class SaveProduct(models.Model):
     full_name = models.CharField(max_length=150,null=True,blank=True)
     phone = models.CharField(max_length=150,null=True,blank=True)
     detailed_data = models.JSONField(null=True,blank=True)
-    comment = models.JSONField(null=True,blank=True)
+    comment = models.TextField(null=True,blank=True)
     files = models.FileField(blank=True,null=True)
     restaurant = models.ForeignKey(Restaurant, on_delete = models.CASCADE,null=True,blank=True)
     created_at = models.DateField(auto_now_add=True)
