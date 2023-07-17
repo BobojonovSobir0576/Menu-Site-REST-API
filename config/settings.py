a0d244ba71cdffa0e44c2ef5d1e2af5e554b84bc
BobojonovSobir0576 <dweb0987654321@gmail.com>
2023-07-17 17:19:38 +0500

    sdsdsd

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from datetime import timedelta
 
-from environs import Env
+
 
 import os
 import sys
@@ -9,8 +9,7 @@ import sys
 from dotenv import load_dotenv
 load_dotenv()
 
-env = Env()
-env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
