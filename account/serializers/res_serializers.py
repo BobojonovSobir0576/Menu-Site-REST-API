f216c9cf74cceab891413d04b349de2e9bb9b10e
BobojonovSobir0576 <dweb0987654321@gmail.com>
2023-07-17 15:51:58 +0500

    sdsdsds

--- account/serializers/res_serializers.py
+++ account/serializers/res_serializers.py
@@ -14,7 +14,7 @@ from account.models import *
 class RestaurantSerializers(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
-        fields = ['__all__']
+        fields = '__all__'
 
 
 class CatalogListSerializers(serializers.ModelSerializer):
