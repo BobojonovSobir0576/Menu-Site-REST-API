8f33307c4e57c059369ceb342bb207da227f942d
BobojonovSobir0576 <dweb0987654321@gmail.com>
2023-07-17 16:52:43 +0500

    finished

--- config/settings.py
+++ config/settings.py
@@ -199,6 +199,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:8000",
     "http://localhost:5173",
     "http://31.129.97.46",
+    "http://www.menu-edge.uz/"
+    "http://menu-edge.uz/"
 ]
 
 
