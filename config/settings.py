c6250cb81b9635ba2dae511ce70dceb78984bba0
BobojonovSobir0576 <dweb0987654321@gmail.com>
2023-07-17 17:40:55 +0500

    sdsds

--- config/settings.py
+++ config/settings.py
@@ -6,11 +6,6 @@ from datetime import timedelta
 import os
 import sys
 
-from dotenv import load_dotenv
-load_dotenv()
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
