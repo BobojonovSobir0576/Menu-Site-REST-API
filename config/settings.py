1e370bf8fb04589823462d5630f62f96d24583a2
BobojonovSobir0576 <dweb0987654321@gmail.com>
2023-08-13 16:38:12 +0500

    add postgresql

--- config/settings.py
+++ config/settings.py
@@ -102,9 +102,6 @@ PAYME: dict = {
 }
 
 
-
-
-
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
@@ -129,10 +126,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'edge_menu',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
